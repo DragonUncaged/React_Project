7d1efcb560239662aabd8a04a55508622b3feccc
DragonUncaged <dragonuncaged@gmail.com>
2021-12-08 09:18:07 +0530

    .

---
+++ 1-birthday-reminder/FinalCode/src/data.js
@@ -0,0 +1,44 @@
+export default [
+  {
+    id: 1,
+    name: 'Urvi Gupta',
+    age: 29,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+  },
+  {
+    id: 2,
+    name: 'Kamala Patil',
+    age: 68,
+    image:
+      'https://media.istockphoto.com/photos/portrait-of-a-beautiful-sixty-year-old-woman-picture-id1202445672?k=20&m=1202445672&s=612x612&w=0&h=r4UyC5UN4fNxFy-NoLkk5QiTlyWFAtjCEKxeXSXrCTQ=',
+  },
+  {
+    id: 3,
+    name: 'Ramesh Kulkarni',
+    age: 36,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+  },
+  {
+    id: 4,
+    name: 'Vasant Korrapati',
+    age: 34,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+  },
+  {
+    id: 5,
+    name: 'Murali Gupta',
+    age: 85,
+    image:
+      'https://media.istockphoto.com/photos/senior-adult-male-laughing-portrait-he-is-90-years-old-picture-id155357459?k=20&m=155357459&s=612x612&w=0&h=SSFaOHLB6Bd9bK5I7SIduvNhO2enUIee5mxH6GukVKk=',
+  },
+  {
+    id: 5,
+    name: 'Chotu',
+    age: 1,
+    image:
+      'https://images.theconversation.com/files/245935/original/file-20181116-194488-19it793.jpg?ixlib=rb-1.1.0&rect=0%2C124%2C1000%2C499&q=45&auto=format&w=1356&h=668&fit=crop',
+  },
+];
